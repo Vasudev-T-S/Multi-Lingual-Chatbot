6186ef282e1158a6671c1d9e78560f11de2f3a1c
Vasudev-T-S-2333 <pachu2333@gmail.com>
2026-09-10 00:21:44 +0530

    Now the message are going to backend

--- MultiChat/chat/views.py
+++ MultiChat/chat/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
 
 # Create your views here.
+def chat_view(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        message = data.get('message')
+        return JsonResponse({'response': "Your Message: " + message})
+    return render(request, 'navigator-chat.html')
